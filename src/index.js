4870482320530f00764cba80bcce84dfb6c9dc8f
Mariya <mariya.poltoratskaya@gmail.com>
2018-09-13 10:54:56 +0300

    "LOVETRIANGLE_1"

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+
+
+    var lovedByFirst = preferences[i];
+    var lovedBySecond = preferences[lovedByFirst - 1];
+
+    if (lovedByFirst === lovedBySecond || lovedBySecond === preferences[lovedBySecond - 1]) {
+      continue;
+    }
+
+    if  (preferences[lovedBySecond - 1] === (i + 1)) {
+      count++;
+    }
+  }
+  return count/3;
 };
